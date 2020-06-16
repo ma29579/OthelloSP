45281c3c2ec89583967089a64cb7e41417c7bc09
ma29579 <maximilian.thomas@live.de>
2020-06-16 18:15:05 +0200

    Kommentare für die Spielbrett-Klasse

--- src/spieler/enheth/SpielSimulation.java
+++ src/spieler/enheth/SpielSimulation.java
@@ -12,7 +12,7 @@ public class SpielSimulation {
     public static void main(String[] args) {
 
         ArrayList<OthelloSpieler> spieler = new ArrayList<>();
-        spieler.add(new Spieler(10));
+        spieler.add(new Spieler());
         spieler.add(new Referenzspieler(6));
 
         new OthelloArena(150, spieler, true);
